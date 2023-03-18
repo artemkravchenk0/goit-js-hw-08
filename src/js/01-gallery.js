8123ac122ceb8b62fe400283d5add30b57f3af90
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-03-18 16:15:40 +0200

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const htmlMarkupArray = galleryItems.map(({preview, original, description}) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`
+)
+
+const galleryEl = document.querySelector('.gallery')
+galleryEl.innerHTML = htmlMarkupArray.join('')
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
 
 console.log(galleryItems);
