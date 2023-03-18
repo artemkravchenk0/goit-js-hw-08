5924a2100ebf524605e1ae6b1b0823bafe042bc5
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-03-18 15:59:36 +0200

    Task 3 (1-3)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    emailEl: document.querySelector('[name=email]'),
+    messageEl: document.querySelector('[name=message]')
+}
+
+
+refs.formEl.addEventListener('input', (evt) => {
+  
+    const formData = {
+        email: refs.emailEl.value,
+        message: refs.messageEl.value
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+
+let formData = localStorage.getItem('feedback-form-state');
+
+if (formData !== null){
+    formData = JSON.parse(formData)
+
+    refs.emailEl.value = formData.email
+    refs.messageEl.value = formData.message
+}
+
+refs.formEl.addEventListener('submit', (evt) =>{
+    evt.preventDefault();
+    let formData = localStorage.getItem('feedback-form-state');
+    console.log(JSON.parse(formData));
+    localStorage.removeItem('feedback-form-state')
+    let form = refs.formEl
+    form.reset()
+})
